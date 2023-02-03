487666a15356093bb160567251d3e3da4d51297b
tirumalavoonna <124362494+tirumalavoonna@users.noreply.github.com>
2023-02-03 18:41:45 +0530

    Update install-pyrequirements.sh

--- install-pyrequirements.sh
+++ install-pyrequirements.sh
@@ -1,5 +1,5 @@
 python3 -m venv venv
-.\venv\Scripts\activate
+source /venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir cython
 python3 -m pip install --no-cache-dir -r /app/python/requirements.txt
