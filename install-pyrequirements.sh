c3d834bce6f451087092857d2f5fa262027e47c6
tirumalavoonna <124362494+tirumalavoonna@users.noreply.github.com>
2023-02-03 17:43:45 +0530

    Create install-pyrequirements.sh

---
+++ install-pyrequirements.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x #echo on
+
+python3 -m venv venv
+source /venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install --no-cache-dir cython
+python3 -m pip install --no-cache-dir -r /app/python/requirements.txt
