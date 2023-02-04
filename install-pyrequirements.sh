6be0985bd56962164f19f87b03bcf77c3dc1fd9e
tirumalavoonna <124362494+tirumalavoonna@users.noreply.github.com>
2023-02-04 10:37:44 +0530

    Update install-pyrequirements.sh

--- install-pyrequirements.sh
+++ install-pyrequirements.sh
@@ -1,4 +1,5 @@
 python3 -m venv venv
 . /venv/bin/activate
 python3 -m pip install --upgrade pip
+python3 -m pip install psycopg2-binary
 python3 -m pip install --no-cache-dir -r /app/python/requirements.txt
